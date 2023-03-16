443e084bc0a5933d5430f9d97a785d5ea647aaa7
sefaerdoğdugil <s.erdogdugil@gmail.com>
2023-03-16 16:34:13 +0300

    Initial commit

---
+++ src/Deneme.java
@@ -0,0 +1,14 @@
+public class Deneme {
+    public static void main(String[] args) {
+        Telefon telefon = new Telefon();
+        telefon.setMarka("Oppo");
+        telefon.setRam(6);
+        telefon.setRenk("Mavi");
+        System.out.println(telefon.getMarka());
+        System.out.println(telefon.getRam());
+        System.out.println(telefon.getRenk());
+    }
+}
+
+
+
---
+++ src/Telefon.java
@@ -0,0 +1,29 @@
+public class Telefon {
+    private int ram;
+    private String marka;
+    private String renk;
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public String getRenk() {
+        return renk;
+    }
+
+    public void setRenk(String renk) {
+        this.renk = renk;
+    }
+}
